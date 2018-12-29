bb301f6fbabde85233511f9ad0c14a22343399e0
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-12-29 23:43:52 +0000

    JSON

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
-import datetime
+import json
 
-x = datetime.datetime.now()
-print(x)
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
