6b42bd4a02e60d6bbc9772ae093a8f789ac1e3cc
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-12-29 23:41:19 +0000

    Stop iterator

--- hello.py
+++ hello.py
@@ -4,15 +4,15 @@ class MyNumbers:
     return self
 
   def __next__(self):
-    x = self.a
-    self.a += 1
-    return x
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
 
 myclass = MyNumbers()
 myiter = iter(myclass)
 
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
+for x in myiter:
+  print(x)
