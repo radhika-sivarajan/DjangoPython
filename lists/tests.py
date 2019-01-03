565d3e02e415699c7cf7017a0b97c64eed3becc0
Radhika Sivarajan <radhikaraj333@gmail.com>
2019-01-03 23:54:09 +0000

    First unit test dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,13 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+class homePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+# class SmokeTest(TestCase):
+#     def test_bad_maths(self):
+#         self.assertEqual(1+1, 2)
