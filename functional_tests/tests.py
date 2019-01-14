4af036785e8df0377de800f245201ac1582fe944
Radhika Sivarajan <radhikaraj333@gmail.com>
2019-01-14 21:49:32 +0000

    new renamed functional test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,11 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -18,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrive_it_later(self):
         # Home page
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # Page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -49,7 +50,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
     
         self.fail('Finish the test')
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
