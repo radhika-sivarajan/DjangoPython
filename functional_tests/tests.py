5f5f33cd58c69571fac7841cacf4cada78ffd338
Radhika Sivarajan <radhikaraj333@gmail.com>
2019-01-18 23:43:49 +0000

    ensuring Regression test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -57,7 +57,28 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        self.wait_for_row_in_list_table(
-            '2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # site remember the list generated a URL
 
         self.fail('Finish the test')
+        
+        # check the URL
+
+        # URL works
+
+    def test_can_start_a_list_for_one_user(self):
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # new user Edith's URL
+        edith_list_url = self.browser,current_url
+        self.assertRegex(edith_list_url,'/lists/.+') 
+
