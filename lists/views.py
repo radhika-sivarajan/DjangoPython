ed3f5553dbc3356aa33f294ff808ca73fa7ddffb
Radhika Sivarajan <radhikaraj333@gmail.com>
2019-02-23 23:04:41 +0000

    Using the Form’s Own Save Method

--- lists/views.py
+++ lists/views.py
@@ -17,36 +17,15 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, "form": form})
 
-   
-    # if request.method == 'POST':
-    #     try:
-    #         item = Item(text=request.POST['text'], list=list_)
-    #         item.full_clean()
-    #         item.save()
-    #         return redirect(list_)
-    #     except ValidationError:
-    #         error = "You can't have an empty list item"
-    # return render(request, 'list.html', {'list': list_, 'error': error})
-
 def new_list(request):
-    form = ItemForm(data=request.POST)  
-    if form.is_valid():  
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
-        return render(request, 'home.html', {"form": form})  
-    # list_ = List.objects.create()
-    # item = Item(text=request.POST['text'], list=list_)
-    # try:
-    #     item.full_clean()
-    #     item.save()
-    # except ValidationError:
-    #     list_.delete()
-    #     error = "You can't have an empty list item"
-    #     return render(request, 'home.html', {"error": error})
-    # return redirect(list_)
+        return render(request, 'home.html', {"form": form})
